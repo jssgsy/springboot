2658cc00460e681209e7d6970f662ec1870beabf
univ <fcsnwu@163.com>
2018-12-25 09:42:12 +0800

    定时任务异步化

--- src/main/java/com/univ/SpringbootApplicationStarter.java
+++ src/main/java/com/univ/SpringbootApplicationStarter.java
@@ -2,6 +2,7 @@ package com.univ;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 // 默认会扫描和注册与SpringbootApplicationStarter同目录及其子目录中的被@Component注解的bean
 @SpringBootApplication(scanBasePackages = {"com.univ"})
 @EnableScheduling   // 开启spring的定时任务功能
+@EnableAsync    // 开启定时任务异步化功能(覆盖默认的任务串行)
 public class SpringbootApplicationStarter {
 
 	public static void main(String[] args) {
--- src/main/java/com/univ/task/TaskDemo.java
+++ src/main/java/com/univ/task/TaskDemo.java
@@ -1,5 +1,6 @@
 package com.univ.task;
 
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Component;
  * @author univ
  * @datetime 2018/12/25 9:17 AM
  * @description 定时任务
+ * 注意：如果不使用@Async，则所有任务都是同步的，即一个任务执行完再执行另一个，都是一个线程执行
  */
 @Component  // 注意这里不要忘了使其为bean
 public class TaskDemo {
@@ -15,11 +17,21 @@ public class TaskDemo {
      * spring boot中被@Scheduled注解的方法就是定时方法，只要开启了spring的定时功能即可
      * @throws InterruptedException
      */
-    @Scheduled(cron = "2 * * * * *")
+    @Scheduled(cron = "1 * * * * *")
+    @Async
     public void sayHello() throws InterruptedException {
         for (int i = 0; i < 10; i++) {
             Thread.sleep(1000);
-            System.out.println("当前执行线程：" + Thread.currentThread().getName() + " , i = " + i);
+            System.out.println("sayHello方法中，当前执行线程：" + Thread.currentThread().getName() + " , i = " + i);
+        }
+    }
+
+    @Scheduled(cron = "1 * * * * *")
+    @Async
+    public void task2() throws InterruptedException {
+        for (int i = 0; i < 10; i++) {
+            Thread.sleep(1000);
+            System.out.println("task2方法中，当前执行线程：" + Thread.currentThread().getName() + " , i = " + i);
         }
     }
 }
