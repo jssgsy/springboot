2188b02be2f8fc0c9a1841ea43484ecfd30a3200
univ <fcsnwu@163.com>
2019-01-08 09:46:59 +0800

    @ConfigurationProperties的使用

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 
 # 功能
 ## spring项目热部署
+## @ConfigurationProperties
+需要引入依赖
+```xml
+<!--使用@ConfigurationProperties需要-->
+<dependency>
+    <groupId>org.springframework.boot</groupId>
+    <artifactId>spring-boot-configuration-processor</artifactId>
+    <optional>true</optional>
+</dependency>
+```
--- src/main/java/com/univ/controller/HelloController.java
+++ src/main/java/com/univ/controller/HelloController.java
@@ -1,7 +1,12 @@
 package com.univ.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.univ.entity.ConfigurationPropertiesDemo;
 
 /**
  * @author univ
@@ -13,10 +18,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/hello")
 public class HelloController {
 
+    @Autowired
+    private ConfigurationPropertiesDemo configurationPropertiesDemo;
+
     @RequestMapping("/hotDeploy")
+    @ResponseBody
     public String hotDeploy() {
         // 这里还没创建视图解析器，先通过curl请求，然后改动这里的内容，观察热部署的效果
         System.out.println("hello, world");
-        return null;
+        return "ok";
+    }
+
+    /**
+     * @ConfigurationProperties的使用
+     * @return
+     */
+    @GetMapping("/configurationProperties")
+    @ResponseBody
+    public ConfigurationPropertiesDemo configuration() {
+        // 千万不要使用new的形式，否则当然获取不到配置的信息，ConfigurationPropertiesDemo的实例已经在bean中了！
+        /*ConfigurationPropertiesDemo configurationPropertiesDemo = new ConfigurationPropertiesDemo();
+        System.out.println(configurationPropertiesDemo.getName());*/
+
+        return configurationPropertiesDemo;
     }
 }
